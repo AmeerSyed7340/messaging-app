3b77a60ff33ffec7b7e2f697424b4cdce9418898
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-03-12 07:08:32 -0400

    Connected client to server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import '../src/assets/styles/Main.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      //Make API call with Fetch API 
+      const response = await fetch('http://localhost:3000', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: message })
+      })
+        .then((response) =>
+          response.json()
+        )
+        .then(data => {
+          console.log(data);
+        });
+
+      //Make sure response is okay
+      // if(response.ok){
+
+      //   setMessage('');
+      // }
+      // else{
+      //   //HTTP error
+      //   console.error('Failed to send message')
+      // }
+    }
+    catch (error) {
+      //handle network errors
+      console.error('Network Error in submitting message: ', error)
+    }
+  }//handleSubmit
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='full-content'>
+        <div className='display'></div>
+        <div className="input-text">
+          <form onSubmit={handleSubmit}>
+            <textarea value={message} onChange={(e) => setMessage(e.target.value)} cols="100" rows="1"></textarea>
+            <button className='btn' type='submit'>Send</button>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {
+  const message = req.body;
+  res.status(201).json({message});
 });
 
 module.exports = router;
